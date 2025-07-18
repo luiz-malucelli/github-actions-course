e925e940867a5006ebb792463d04cf652f2146bd
empatia-dev <info@jogoempatia.com>
2025-07-18 20:27:46 -0300

    feat(safe-title-check): refactor index.js

--- .github/actions/safe-title-check/index.js
+++ .github/actions/safe-title-check/index.js
@@ -1,14 +1,17 @@
 const core = require('@actions/core');
 
 async function run() {
-    const prTitle = core.getInput('pr-title', { required: true }).trim();
-
-    const prRegex = /^feat/;
-
-    if (prRegex.test(prTitle)) {
-        core.info('PR is a feature');
-    } else {
-        core.info('PR is not a feature');
+    try {
+        const prTitle = core.getInput('pr-title', { required: true }).trim();
+        const prRegex = /^feat/;
+    
+        if (prRegex.test(prTitle)) {
+            core.info('PR is a feature');
+        } else {
+            core.setFailed('PR is not a feature');
+        }
+    } catch (e) {
+        core.setFailed(e.message);
     }
 }
 
